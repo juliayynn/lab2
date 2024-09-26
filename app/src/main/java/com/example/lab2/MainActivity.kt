833eb679b9d9f91c3449da471ff7132eddeef1b9
user <yunusova1203@gmail.com>
2024-09-26 21:38:12 +0700

    Инициализация элементов

---
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.lab2
+
+import kotlin.math.sin
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var editTextNumberDecimal: EditText
+    private lateinit var button2: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
