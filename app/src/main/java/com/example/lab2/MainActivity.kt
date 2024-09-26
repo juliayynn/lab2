36e01c8382305ba8e8adb0a43b34db0b747778cc
user <yunusova1203@gmail.com>
2024-09-26 22:04:40 +0700

    Готовый цикл

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -7,24 +7,50 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var editTextNumberDecimal: EditText
-    private lateinit var button2: Button
+    private lateinit var editText: EditText
+    private lateinit var button: Button
+    private lateinit var label: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        editText = findViewById(R.id.editTextNumberDecimal)
+        button = findViewById(R.id.button2)
+        label = findViewById(R.id.textView4)
 
 
+        button.setOnClickListener { function_sin() }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    private fun function_sin(){
+        val N = editText.text.toString()
+        val result = label
+        val n = N.toInt()
+        var tmp = 0.0
+        var m = 0.0
+
+
+        for (i in 1..n){
+            m = 0.0
+            for(j in 1..i){
+                val transformJ = j.toDouble()
+                m += sin(transformJ)
+
+
+            }
+            tmp += (1.0/m)
+        }
+        result.text = "Результат: $tmp"
     }
 }
